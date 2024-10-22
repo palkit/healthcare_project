5fcc9059b0dbf44d426c4d283e18388fd942d406
palkit <dhiman.proff@gmail.com>
2024-10-22 15:41:18 +0530

    2nd commit

--- server/server.js
+++ server/server.js
@@ -1,25 +1,27 @@
-//Framework Configuration
-const express = require("express");
+// Import express
+const express = require('express');
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
 
-// connectDb();
+//env file config
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json())
+app.use(cors());
 
-// Error handling middleware
-// app.use(errorHandler);
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Working......');
+});
 
-// Router Below
-app.get('/',(req,res)=>{
-    res.send("Working......");
-})
-
-// APP Config Start
+// Start the server on port 3000
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
-})
+    console.log(`Server running on http://localhost:${port}`);
+});
